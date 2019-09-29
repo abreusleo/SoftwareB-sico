fb38c63f989b84f93486dcd5f19903a931b903a7
jmbessa <54651955+jmbessa@users.noreply.github.com>
2019-09-29 14:26:36 -0300

    Create bigint.c

---
+++ bigint.c
@@ -0,0 +1,17 @@
+//JOÃO MARCELLO BESSA RODRIGUES - 1720539 - 3WA
+//LEONARDO DOS SANTOS ABREU - XXXXXXXX - 3WB
+
+#include "bigint.h"
+#include <stdio.h>
+
+
+/* Auxiliares */
+
+
+/* Principais */
+
+void big_comp2(BigInt res, BigInt a) {
+
+
+
+}
