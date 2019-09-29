93f76bc99c065680e6a40bfe8c8c7de4e9744066
abreusleo <37474088+abreusleo@users.noreply.github.com>
2019-09-29 14:31:43 -0300

    Update bigint.c

--- Trabalho SB/Trabalho SB/bigint.c
+++ Trabalho SB/Trabalho SB/bigint.c
@@ -1,3 +1,18 @@
-//JO√O MARCELLO BESSA RODRIGUES - 1720539 - 3WA
-//LEONARDO DOS SANTOS ABREU - XXXXXXXX - 3WB
+//JO√ÉO MARCELLO BESSA RODRIGUES - 1720539 - 3WA
+//LEONARDO DOS SANTOS ABREU - 1720565 - 3WB
 
+//eae men
+
+#include <stdio.h>
+void dump (void *p, int n) {
+ unsigned char *p1 = p;
+ while (n--) {
+ printf("%p - %02x\n", p1, *p1);
+ p1++;
+ }
+}
+int main() {
+ int num = 0x01020304;
+dump(&num, sizeof(int));
+ return 0;
+}
