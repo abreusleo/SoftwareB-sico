21ee1f63fbfce95e55c65c541d80705adc87cd4e
abreusleo <37474088+abreusleo@users.noreply.github.com>
2019-09-30 12:30:48 -0300

    Update teste.c
    Alterei a main e deixei a *sua* comentada. Faz o teste com essa que funcionou para o número 8

--- Trabalho SB/Trabalho SB/teste.c
+++ Trabalho SB/Trabalho SB/teste.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "bigint.h"
 
+/*
+COMENTEI A SUA MAIN, SEGUI O MESMO MODELO QUE ELES USARAM PARA TESTAR
 int main(void) {
 	long val = 8;
 	BigInt a;
@@ -11,4 +13,18 @@ int main(void) {
 	dump(a, val);
 
 	return 0;
+}
+*/
+
+int main(void) {
+ long val1 = 8;
+    BigInt a;
+
+    big_val(a, val1);
+
+    printf("Valor da dump usando big_val: %016lx\n", val1);
+    printf("Função dump normal:\n");	
+    dump(a, 16);
+
+    return 0;
 }
