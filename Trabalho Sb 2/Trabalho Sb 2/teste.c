06b0ba3211bb309dcfa96db8cd12e4f94285f934
jmbessa <54651955+jmbessa@users.noreply.github.com>
2019-10-05 22:03:49 -0300

    Update teste.c

--- Trabalho Sb 2/Trabalho Sb 2/teste.c
+++ Trabalho Sb 2/Trabalho Sb 2/teste.c
@@ -1,17 +1,22 @@
+
 #include <stdio.h>
 #include "bigint.h"
 
 int main(void) {
     
-    long val1 = 10, n = 3;
+    long val1 = 1, n = 8;
     // int shift = 13;
-    BigInt a, b, res;
+    BigInt a, res, res2;
 
     big_val(a, val1);
-
     print_bigInt(a);
-	big_sar(res, a, n);
-    dump(res, 16);
+    big_shl(res, a, n);
+    printf("\n\n");
+    
+    print_bigInt(res);
+    
+    big_sar(res2, res, n);
+    print_bigInt(res2); 
 
     return 0;
 }
